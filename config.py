1355920cfe792b255b0eb7ad5a233a537183a1eb
tengkd <130631162+tengkd@users.noreply.github.com>
2023-04-13 12:44:34 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cm7btrohxhxc.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tengkaideng-employee"
+customregion = "us-east-1"
